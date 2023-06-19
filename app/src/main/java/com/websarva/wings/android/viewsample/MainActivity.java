f0cd185668d9f09f162c8d1551625f56e5f1850f
yuyash0106 <137029519+yuyash0106@users.noreply.github.com>
2023-06-19 17:09:47 +0900

    Initial commit

---
+++ app/src/main/java/com/websarva/wings/android/viewsample/MainActivity.java
@@ -0,0 +1,13 @@
+package com.websarva.wings.android.viewsample;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
